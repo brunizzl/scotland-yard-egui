0a49b51a347cba0351371ffaf9ed2100314b73ad
Bruno <46483121+brunizzl@users.noreply.github.com>
2023-11-08 00:15:20 +0100

    convex hull can be drawn

--- src/app.rs
+++ src/app.rs
@@ -1,40 +1,44 @@
 
-use std::default;
-
 use egui::*;
 
 use super::graph;
 
 
-pub enum GraphShape { Hexagon, Pentagon, }
+pub enum GraphShape { Hexagon(Vec<graph::hex_pos::HexPos>), Pentagon, }
 
 //denotes eighter a cop or the robber as node on screen
 //Pos2 is in graph coordinates, not in screen coordinates
-struct CharacterPos {
-    nearest_node: usize,
+pub struct CharacterPos {
+    pub nearest_node: usize,
+    on_node: bool,
     pos: Pos2,
 }
 
 impl Default for CharacterPos {
     fn default() -> Self {
-        CharacterPos { nearest_node: 0, pos: pos2(0.1, 0.1) }
+        CharacterPos { nearest_node: 0, on_node: false, pos: pos2(0.1, 0.1) }
     }
 }
 
 impl CharacterPos {
     //go through all neighbors of current nearest node, if any neigbor is closer than current nearest node, change to that neighbor
     //(converges to globally nearest node only for "convex" graphs)
-    fn fast_update_pos(&mut self, map: &graph::Graph) -> f32 {
+    fn fast_update_pos(&mut self, map: &graph::Graph) -> bool {
         let nearest_pos = map.nodes()[self.nearest_node].pos;
         let mut nearest_dist = (nearest_pos - self.pos).length_sq();
+        let mut change = false;
         for (i, neigh) in map.neigbors_of(self.nearest_node) {
             let neigh_dist = (neigh.pos - self.pos).length_sq();
             if neigh_dist < nearest_dist {
                 self.nearest_node = i;
                 nearest_dist = neigh_dist;
+                change = true;
             }
         }
-        f32::sqrt(nearest_dist)
+        let now_on_node = nearest_dist <= 0.01;
+        change |= now_on_node != self.on_node;
+        self.on_node = now_on_node;
+        change
     }
 }
 
@@ -43,25 +47,47 @@ pub struct State {
     map_shape: GraphShape,
     map_radius: usize,
 
-    nr_cops: usize,
+    show_robber_closer: bool,
+    show_convex_hull: bool,
+    show_escapeable_nodes: bool, //all nodes closer to the outside of the convex hull than to the next cop
+
     cops: Vec<CharacterPos>,
     robber: CharacterPos,
 }
 
 impl State {
+
+    fn resize(&mut self, radius: usize) {
+        let (map, names) = graph::Graph::new_plane_tiles_hexagon(radius, Pos2::new(1.0, 1.0), 1.0);
+        self.map_shape = GraphShape::Hexagon(names);
+        self.map = map;
+        self.map_radius = radius;        
+        self.robber.nearest_node = 0;
+        for cop in &mut self.cops {
+            cop.nearest_node = 0;
+        }
+    }
+
     /// Called once before the first frame.
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        let radius = 5;
+        let mut res = State { 
+            map: graph::Graph::empty(),
+            map_shape: GraphShape::Hexagon(Vec::new()),
+            map_radius: 0,
 
-        State { 
-            map: graph::Graph::new_plane_tiles_hexagon(radius, Pos2::new(1.0, 1.0), 1.0),
-            map_shape: GraphShape::Hexagon,
-            map_radius: radius,
+            show_robber_closer: true,
+            show_convex_hull: true,
+            show_escapeable_nodes: true,
 
-            nr_cops: 0,
             cops: Vec::new(),
             robber: CharacterPos::default(),
-         }
+         };
+         res.resize(5);
+         res
+    }
+
+    fn active_cops(&self) -> Vec<usize> {
+        self.cops.iter().filter(|c| c.on_node).map(|c| c.nearest_node).collect()
     }
 }
 
@@ -82,7 +108,7 @@ impl eframe::App for State {
 
 
         SidePanel::left("left_panel").show(ctx, |ui| {
-            ui.vertical_centered(|ui| {
+            ui.vertical(|ui| {
                 ui.heading("Optionen");
 
                 { //adjust radius
@@ -91,19 +117,17 @@ impl eframe::App for State {
                         ui.label("Radius: ");
                         ui.add(DragValue::new(&mut new_radius));
                     });
-                    new_radius = usize::max(2, usize::min(new_radius, 100));
+                    new_radius = usize::max(1, usize::min(new_radius, 100));
                     if new_radius != self.map_radius {
-                        self.map_radius = new_radius;
-                        self.map = graph::Graph::new_plane_tiles_hexagon(self.map_radius, Pos2::new(1.0, 1.0), 1.0);
-                        self.robber.nearest_node = 0;
-                        for cop in &mut self.cops {
-                            cop.nearest_node = 0;
-                        }
+                        self.resize(new_radius);
                     }
                 }
-                if ui.button("Neuer Cop").clicked() {
+                if ui.button("neuer Cop").clicked() {
                     self.cops.push(CharacterPos::default());
                 }
+                ui.add(Checkbox::new(&mut self.show_robber_closer, "markiere für Räuber\n nähere Knoten"));
+                ui.add(Checkbox::new(&mut self.show_convex_hull, "zeige Convexe Hüll\n um Cops"));
+                ui.add(Checkbox::new(&mut self.show_escapeable_nodes, "zeige Punkte näher an\n Hüllenrand als an Cops"));
             });
         });
 
@@ -131,30 +155,63 @@ impl eframe::App for State {
                 painter.add(line);
             }
 
+            if self.show_robber_closer {
+                let robber_closer_color = Color32::from_rgb(80, 210, 80);
+                for v in self.map.nodes() {
+                    if v.robber_advantage > 0 {
+                        let draw_pos = to_screen.transform_pos(v.pos);
+                        let marker_circle = Shape::circle_filled(draw_pos, 4.0, robber_closer_color);
+                        painter.add(marker_circle);
+                    }
+                }
+            }
+            if self.show_convex_hull {
+                let stroke = Stroke::new(2.0, Color32::from_rgb(100, 100, 220));
+                for v in self.map.nodes() {
+                    if graph::Bool3::Yes == v.in_convex_hull  {
+                        let draw_pos = to_screen.transform_pos(v.pos);
+                        let marker_circle = Shape::circle_stroke(draw_pos, 6.0, stroke);
+                        painter.add(marker_circle);
+                    }
+                }
+            }
+
             let characters = std::iter::once(&mut self.robber).chain(self.cops.iter_mut());
+            let mut change = false;
             for (i, character) in characters.enumerate() {                
                 let character_size = 8.0;
 
-                let drawn_pos = to_screen.transform_pos(character.pos);
-                let point_rect = Rect::from_center_size(drawn_pos, vec2(character_size * 3.0, character_size * 3.0));
-                let character_id = response.id.with(i);
-                let point_response = ui.interact(point_rect, character_id, Sense::drag());
-                character.pos = to_screen.inverse().transform_pos(drawn_pos + point_response.drag_delta());
-
-                let dist_closest = character.fast_update_pos(&self.map);
-                if dist_closest <= 0.05 {
+                let real_screen_pos = to_screen.transform_pos(character.pos);
+                let draw_pos = if character.on_node {
                     let nearest_node_pos = to_screen.transform_pos(self.map.nodes()[character.nearest_node].pos);
                     let marker_circle = Shape::circle_stroke(nearest_node_pos, character_size + 4.0, edge_stroke);
                     painter.add(marker_circle);
+                    nearest_node_pos
                 }
+                else {
+                    real_screen_pos
+                };
+                let point_rect = Rect::from_center_size(draw_pos, vec2(character_size * 3.0, character_size * 3.0));
+                let character_id = response.id.with(i);
+                let point_response = ui.interact(point_rect, character_id, Sense::drag());
+                character.pos = to_screen.inverse().transform_pos(real_screen_pos + point_response.drag_delta());
+
+                change |= character.fast_update_pos(&self.map);
 
                 let fill_color = if i == 0 {
                     Color32::from_rgb(200, 50, 50) //robber
                 } else {
                     Color32::from_rgb(10, 50, 190) //cop
                 };
-                let character_circle = Shape::circle_filled(drawn_pos, character_size, fill_color);
+                let character_circle = Shape::circle_filled(draw_pos, character_size, fill_color);
                 painter.add(character_circle);
+            }
+            if change {
+                let active_cops = self.active_cops();
+                self.map.calc_robber_advantage(self.robber.nearest_node, &active_cops);
+                if let GraphShape::Hexagon(names) = &self.map_shape {
+                    self.map.calc_hex_convex_police_hull(&names, &active_cops);
+                }
             }
         });
     }
