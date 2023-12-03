e352dc5a08a801125f74e2d0eb044b8618c28b61
Bruno <46483121+brunizzl@users.noreply.github.com>
2023-12-03 19:37:43 +0100

    readd web support

--- src/main.rs
+++ src/main.rs
@@ -17,3 +17,23 @@ fn main() -> eframe::Result<()> {
         Box::new(|cc| Box::new(scotland_yard_egui::State::new(cc))),
     )
 }
+
+// When compiling to web using trunk:
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "the_canvas_id", // hardcode it
+                web_options,
+                Box::new(|cc| Box::new(scotland_yard_egui::State::new(cc))),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
+}
