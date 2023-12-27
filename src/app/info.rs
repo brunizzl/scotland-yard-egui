b23d609ac3338e984a81ecc6e24107c89233a87d
Bruno <46483121+brunizzl@users.noreply.github.com>
2023-12-27 14:02:27 +0100

    extract Info's options to own struct

--- src/app/info.rs
+++ src/app/info.rs
@@ -27,21 +27,10 @@ pub enum DrawNumbers { None, Indices, RobberAdvantage, EscapeableNodes, MinCopDi
 #[derive(Clone, Copy, PartialEq, serde::Deserialize, serde::Serialize)]
 pub enum RobberStrat { None, EscapeHullNonLazy }
 
-pub struct Info {
-    //state kept for each node in map
-    cop_hull_data: ConvexHullData,
-    escapable: EscapeableNodes,
-    min_cop_dist: Vec<isize>, //elementwise minimum of .distance of active cops in self.characters
-    cop_advantage: Vec<isize>,
-    pub marked_manually: Vec<bool>,
-
-    /// this is only used as intermediary variable during computations. 
-    /// to not reallocate between frames/ algorithms, the storage is kept and passed to where needed.
-    pub queue: VecDeque<usize>,
-    
-    pub characters: CharacterState,
-
+#[derive(Clone, Copy, PartialEq, serde::Deserialize, serde::Serialize)]
+struct Options {
     robber_info: RobberInfo,
+
     //both are only used, when the respective RobberInfo is active
     marked_cop_dist: isize, //determines cop dist marked in RobberInfo::CopDist
     small_robber_dist: isize, //determines max dist marked in RobberInfo::SmallRobberDist
@@ -49,79 +38,23 @@ pub struct Info {
     robber_strat: RobberStrat,
     vertex_info: DrawNumbers,
     show_convex_hull: bool,
-    menu_change: bool,
+    show_hull_boundary: bool,
 }
 
-mod storage_keys {
-    pub const CHARACTERS: &'static str = "app::info::characters";
-    pub const MARKED_MANUALLY: &'static str = "app::info::manually_marked";
-    pub const ROBBER_INFO: &'static str = "app::info::robber_info";
-    pub const ROBBER_STRAT: &'static str = "app::info::robber_strat";
-    pub const VERTEX_INFO: &'static str = "app::info::vertex_info";
-    pub const SHOW_HULL: &'static str = "app::info::show_convex_hull";
-}
-
-impl Info {
-    pub fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        //idea: only store settings, not state that is recomputable.
-        let all_dists = self.characters.all_mut().iter_mut()
-            .map(|c| std::mem::take(&mut c.distances)).collect_vec();
-        use storage_keys::*;
-        eframe::set_value(storage, CHARACTERS, &self.characters);
-        for (ch, dists) in izip!(self.characters.all_mut(), all_dists.into_iter()) {
-            ch.distances = dists;
-        }
-
-        if self.marked_manually.iter().any(|&x| x) {
-            eframe::set_value(storage, MARKED_MANUALLY, &self.marked_manually);
-        }
-        eframe::set_value(storage, ROBBER_INFO, &self.robber_info);
-        eframe::set_value(storage, ROBBER_STRAT, &self.robber_strat);
-        eframe::set_value(storage, VERTEX_INFO, &self.vertex_info);
-        eframe::set_value(storage, SHOW_HULL, &self.show_convex_hull);
-    }
-
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        use storage_keys::*;
-        let characters = load_or(cc.storage, CHARACTERS, CharacterState::new);
-        let marked_manually = load_or(cc.storage, MARKED_MANUALLY, Vec::new);
-        let robber_info = load_or(cc.storage, ROBBER_INFO, || RobberInfo::None);
-        let robber_strat = load_or(cc.storage, ROBBER_STRAT, || RobberStrat::None);
-        let vertex_info = load_or(cc.storage, VERTEX_INFO, || DrawNumbers::None);
-        let show_convex_hull = load_or(cc.storage, SHOW_HULL, || false);
-
-        Self { 
-            cop_hull_data: ConvexHullData::new(),
-            escapable: EscapeableNodes::new(),
-            min_cop_dist: Vec::new(),
-            cop_advantage: Vec::new(),
-            marked_manually,
-
-            queue: VecDeque::new(),
-            
-            characters,
-
-            robber_info,
-            small_robber_dist: 10,
-            marked_cop_dist: 0,
-
-            robber_strat,
-            vertex_info,
-            show_convex_hull,
-            menu_change: false,
-        }
-    }
-
-    pub fn draw_menu(&mut self, ui: &mut Ui, map: &map::Map) {
-        self.menu_change = false;
+impl Options {
+    pub fn draw_menu(&mut self, ui: &mut Ui) -> bool {
+        let mut menu_change = false;
         ui.collapsing("Knoteninfo", |ui|{
-            self.menu_change |= 
+            menu_change |= 
                 ui.add(Checkbox::new(&mut self.show_convex_hull, "zeige Konvexe Hülle um Cops"))
                 .on_hover_text("Berechnung des Randes kann aus Effizienzgründen für sehr kleine / \
                 sehr dünne Hüllen fehlerhaft sein. \n\
                 Wenn die Cops im 3D Fall den gesamten Graphen durch die Hülle abdecken, wird trotzdem ein Rand gezeigt, da\
                 der Punkt am weitesten entfernt von jedem Cop vom Algorithmus hier immer als außerhalb der Hülle angenommen wird.")
                 .changed();
+            menu_change |= ui.add(Checkbox::new(&mut self.show_hull_boundary, "zeige Grenze"))
+                .changed();
+
 
             ui.add_space(5.0);
             ui.label("Marker:");
@@ -165,7 +98,7 @@ impl Info {
                 "Debugging")
                 .on_hover_text("Was auch immer gerade während des letzten mal kompilierens interessant war");
 
-            self.menu_change |= old != self.robber_info;
+            menu_change |= old != self.robber_info;
         });
         ui.collapsing("Zahlen", |ui|{
             let old = self.vertex_info;
@@ -194,7 +127,7 @@ impl Info {
                 .on_hover_text("Überraschungsinfo, die zum letzten Kompilierzeitpunkt \
                 gerade spannend zum debuggen war");
 
-            self.menu_change |= old != self.vertex_info;
+            menu_change |= old != self.vertex_info;
         });
         ui.collapsing("Strategie Räuber", |ui|{
             let old = self.robber_strat;
@@ -204,10 +137,98 @@ impl Info {
             ui.radio_value(&mut self.robber_strat, RobberStrat::EscapeHullNonLazy, 
                 "Entkomme Hülle")
                 .on_hover_text("Heuristik für Fluchtoption (1)");
-            self.menu_change |= old != self.robber_strat;
+            menu_change |= old != self.robber_strat;
         });
-        self.menu_change |= 
-            self.characters.draw_menu(ui, map, &mut self.queue);
+        menu_change
+    }
+}
+
+pub struct Info {
+    //state kept for each node in map
+    cop_hull_data: ConvexHullData,
+    escapable: EscapeableNodes,
+    min_cop_dist: Vec<isize>, //elementwise minimum of .distance of active cops in self.characters
+    cop_advantage: Vec<isize>,
+    pub marked_manually: Vec<bool>,
+
+    /// this is only used as intermediary variable during computations. 
+    /// to not reallocate between frames/ algorithms, the storage is kept and passed to where needed.
+    pub queue: VecDeque<usize>,
+    
+    pub characters: CharacterState,
+
+    options: Options,
+    menu_change: bool,
+}
+
+mod storage_keys {
+    pub const OPTIONS: &'static str = "app::info::options";
+    pub const CHARACTERS: &'static str = "app::info::characters";
+    pub const MARKED_MANUALLY: &'static str = "app::info::manually_marked";
+    pub const ROBBER_INFO: &'static str = "app::info::robber_info";
+    pub const ROBBER_STRAT: &'static str = "app::info::robber_strat";
+    pub const VERTEX_INFO: &'static str = "app::info::vertex_info";
+    pub const SHOW_HULL: &'static str = "app::info::show_convex_hull";
+    pub const SHOW_HULL_BND: &'static str = "app::info::show_hull_boundary";
+}
+
+impl Info {
+    pub fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        //idea: only store settings, not state that is recomputable.
+        let all_dists = self.characters.all_mut().iter_mut()
+            .map(|c| std::mem::take(&mut c.distances)).collect_vec();
+        use storage_keys::*;
+        eframe::set_value(storage, CHARACTERS, &self.characters);
+        for (ch, dists) in izip!(self.characters.all_mut(), all_dists.into_iter()) {
+            ch.distances = dists;
+        }
+
+        if self.marked_manually.iter().any(|&x| x) {
+            eframe::set_value(storage, MARKED_MANUALLY, &self.marked_manually);
+        }
+        eframe::set_value(storage, OPTIONS, &self.options);
+    }
+
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        use storage_keys::*;
+        let characters = load_or(cc.storage, CHARACTERS, CharacterState::new);
+        let marked_manually = load_or(cc.storage, MARKED_MANUALLY, Vec::new);
+
+        let robber_info = load_or(cc.storage, ROBBER_INFO, || RobberInfo::None);
+        let robber_strat = load_or(cc.storage, ROBBER_STRAT, || RobberStrat::None);
+        let vertex_info = load_or(cc.storage, VERTEX_INFO, || DrawNumbers::None);
+        let show_convex_hull = load_or(cc.storage, SHOW_HULL, || false);
+        let show_hull_boundary = load_or(cc.storage, SHOW_HULL_BND, || false);
+
+        let options = load_or(cc.storage, OPTIONS, || Options {
+            robber_info,
+            marked_cop_dist: 10,
+            small_robber_dist: 10,
+            robber_strat,
+            vertex_info,
+            show_convex_hull,
+            show_hull_boundary,
+        });
+
+        Self { 
+            cop_hull_data: ConvexHullData::new(),
+            escapable: EscapeableNodes::new(),
+            min_cop_dist: Vec::new(),
+            cop_advantage: Vec::new(),
+            marked_manually,
+
+            queue: VecDeque::new(),
+            
+            characters,            
+            options,
+            menu_change: false,
+        }
+    }
+
+    pub fn draw_menu(&mut self, ui: &mut Ui, map: &map::Map) {
+        self.menu_change = false;
+        self.menu_change |= self.options.draw_menu(ui);
+        self.menu_change |= self.characters.draw_menu(ui, map, &mut self.queue);
     }
 
     fn update_min_cop_dist(&mut self, edges: &EdgeList) {
@@ -299,24 +320,25 @@ impl Info {
         let robber_moved = self.characters.robber_updated();
         let cop_moved = self.characters.active_cop_updated();
 
-        let update_cop_advantage = self.robber_info == RobberInfo::RobberAdvantage
-            || self.robber_info == RobberInfo::Debugging
-            || self.vertex_info == DrawNumbers::RobberAdvantage
-            || self.vertex_info == DrawNumbers::Debugging;
+        let update_cop_advantage = self.options.robber_info == RobberInfo::RobberAdvantage
+            || self.options.robber_info == RobberInfo::Debugging
+            || self.options.vertex_info == DrawNumbers::RobberAdvantage
+            || self.options.vertex_info == DrawNumbers::Debugging;
             
-        let update_escapable = self.robber_info == RobberInfo::EscapeableNodes
-            || self.robber_info == RobberInfo::Debugging
-            || self.vertex_info == DrawNumbers::EscapeableNodes
-            || self.vertex_info == DrawNumbers::Debugging;
+        let update_escapable = self.options.robber_info == RobberInfo::EscapeableNodes
+            || self.options.robber_info == RobberInfo::Debugging
+            || self.options.vertex_info == DrawNumbers::EscapeableNodes
+            || self.options.vertex_info == DrawNumbers::Debugging;
         
         let update_hull = update_cop_advantage 
             || update_escapable 
-            || self.show_convex_hull;
+            || self.options.show_convex_hull
+            || self.options.show_hull_boundary;
 
         let update_min_cop_dist = update_hull
-            || self.robber_info == RobberInfo::CopDist
-            || self.robber_info == RobberInfo::NearNodes
-            || self.vertex_info == DrawNumbers::MinCopDist;
+            || self.options.robber_info == RobberInfo::CopDist
+            || self.options.robber_info == RobberInfo::NearNodes
+            || self.options.vertex_info == DrawNumbers::MinCopDist;
 
         let nr_vertices = con.edges.nr_vertices();
         if (cop_moved || self.min_cop_dist.len() != nr_vertices) && update_min_cop_dist {
@@ -372,7 +394,7 @@ impl Info {
     }
 
     fn draw_convex_cop_hull(&self, con: &DrawContext<'_>) {
-        if !self.show_convex_hull {
+        if !self.options.show_convex_hull {
             return;
         }
         for (&in_hull, &pos, &vis) in izip!(self.cop_hull_data.hull(), con.positions, con.visible) {
@@ -382,13 +404,14 @@ impl Info {
                 con.painter.add(marker_circle);
             }
         }
-        for seg in self.cop_hull_data.boundary_interieurs() {
-            for &v in seg {
-                if con.visible[v] {
-                    let draw_pos = con.vertex_draw_pos(v);
-                    let marker_circle = Shape::circle_filled(draw_pos, con.scale * 2.0, WHITE);
-                    con.painter.add(marker_circle);
-                }
+        if !self.options.show_hull_boundary {
+            return;
+        }
+        for &v in self.cop_hull_data.boundary() {
+            if con.visible[v] {
+                let draw_pos = con.vertex_draw_pos(v);
+                let marker_circle = Shape::circle_filled(draw_pos, con.scale * 2.0, WHITE);
+                con.painter.add(marker_circle);
             }
         }
     } 
@@ -399,7 +422,7 @@ impl Info {
             let marker_circle = Shape::circle_filled(draw_pos, con.scale * 6.0, color);
             con.painter.add(marker_circle);
         };
-        match (self.robber_info, self.characters.robber()) {
+        match (self.options.robber_info, self.characters.robber()) {
             (RobberInfo::NearNodes, Some(r)) => 
                 for (r_dist, c_dist, &pos, &vis) in 
                 izip!(&r.distances, &self.min_cop_dist, con.positions, con.visible) {
@@ -416,7 +439,7 @@ impl Info {
                 },
             (RobberInfo::SmallRobberDist, Some(r)) => {
                 let bnd = RobberInfo::scale_small_dist_with_resolution(
-                    self.small_robber_dist, con.resolution);
+                    self.options.small_robber_dist, con.resolution);
                 for (&dist, &pos, &vis) in izip!(&r.distances, con.positions, con.visible) {
                     if vis && dist <= bnd {
                         draw_circle_at(pos, GREEN);
@@ -425,7 +448,7 @@ impl Info {
             },
             (RobberInfo::CopDist, _) => 
             for (&dist, &pos, &vis) in izip!(&self.min_cop_dist, con.positions, con.visible) {
-                if vis && dist == self.marked_cop_dist {
+                if vis && dist == self.options.marked_cop_dist {
                     draw_circle_at(pos, GREEN);
                 }
             }
@@ -447,7 +470,7 @@ impl Info {
     }    
 
     fn draw_numbers(&self, ui: &Ui, con: &DrawContext<'_>) {
-        if self.vertex_info == DrawNumbers::None {
+        if self.options.vertex_info == DrawNumbers::None {
             return;
         }
         let true_bits = |x:u32| -> String {
@@ -460,7 +483,7 @@ impl Info {
         let color = if ui.ctx().style().visuals.dark_mode { WHITE } else { BLACK };
         for (i, &pos, &vis) in izip!(0.., con.positions, con.visible) {
             if vis {
-                let txt = match self.vertex_info {
+                let txt = match self.options.vertex_info {
                     DrawNumbers::Indices => { i.to_string() }
                     DrawNumbers::MinCopDist => { self.min_cop_dist[i].to_string() }
                     DrawNumbers::None => { panic!() }
@@ -483,10 +506,10 @@ impl Info {
     }
 
     fn draw_robber_strat(&self, con: &DrawContext<'_>) {
-        if self.robber_strat == RobberStrat::None {
+        if self.options.robber_strat == RobberStrat::None {
             return;
         }
-        let potential = |v| match self.robber_strat {
+        let potential = |v| match self.options.robber_strat {
             RobberStrat::None => panic!(),
             RobberStrat::EscapeHullNonLazy => {
                 //the robber will always try to increase the robber advantage, except for when that would lead to beeing
