927c2340efa1e51a7fde6d6dbdff1f1d4b65a15c
Bruno <46483121+brunizzl@users.noreply.github.com>
2024-12-18 00:28:18 +0100

    small changes to dilemma

--- src/graph/hull/escapable_extra.rs
+++ src/graph/hull/escapable_extra.rs
@@ -105,8 +105,6 @@ impl DilemmaNodes {
             debug_assert!(escape_dirs.connected_on(Norm::Hex));
             debug_assert_eq!(escape_dirs.0.count_ones(), 2);
 
-            self.energy.fill(isize::MIN);
-
             overlapping_left[component_v] -= component_bit;
             queue.push_back(component_v);
 
@@ -152,11 +150,13 @@ impl DilemmaNodes {
                     };
                     min_diff = isize::min(min_diff, diff_dir.abs());
                 }
-                min_diff + 1
+                min_diff
             };
 
+            // these are the four directions of component_dirs as coords and
+            // always ordered such that directions at neighboring indices are adjacent
             let all_relevant_coords = {
-                let mut res = [Coords { x: 0, y: 0 }; 4];
+                let mut res = [Coords { x: isize::MAX, y: isize::MAX }; 4];
                 let vals = Dirs::all_bits_and_directions(Norm::Hex)
                     .cycle()
                     .skip_while(|(&dir, _)| component_dirs.intersection(dir).nonempty())
@@ -165,52 +165,54 @@ impl DilemmaNodes {
                     debug_assert!(component_dirs.intersection(dir).nonempty());
                     *ptr = val;
                 }
+                debug_assert!(res
+                    .iter()
+                    .tuple_windows()
+                    .all(|(&d1, &d2)| Norm::Hex.apply(d1 - d2) == 1));
                 res
             };
+            // the coordinates of the two directions in escape_dirs
             let esc_dir_coords = [all_relevant_coords[1], all_relevant_coords[2]];
+            // the two "outher" directions
             let parallel_dir_coords = [all_relevant_coords[0], all_relevant_coords[3]];
             debug_assert_eq!(parallel_dir_coords[0], -parallel_dir_coords[1]);
-            debug_assert_eq!(
-                esc_dir_coords[0] + parallel_dir_coords[1],
-                esc_dir_coords[1]
-            );
+
+            self.energy.fill(isize::MIN);
             queue.push_back(component_v);
-            'pop_queue: while let Some(v) = queue.pop_front() {
+            'compute_energy: while let Some(v) = queue.pop_front() {
                 if self.energy[v] != isize::MIN {
-                    continue 'pop_queue;
+                    continue 'compute_energy;
                 }
                 if min_cop_dist[v] < 2 {
-                    continue 'pop_queue;
+                    continue 'compute_energy;
                 }
-                let v_dirs = cone_dirs[v].intersection(component_dirs);
-                // v is part of the original overlap
-                if v_dirs == component_dirs {
+                if cone_dirs[v].intersection(component_dirs) == component_dirs {
+                    // v is part of the original overlap
                     self.energy[v] = thickness;
                     self.dilemma_regions[v] |= component_bit;
                     self.dilemma_dirs[v].unionize(component_dirs);
 
                     queue.extend(edges.neighbors_of(v));
-                    continue 'pop_queue;
+                    continue 'compute_energy;
                 }
-                let v_coords = g.coordinates_of(v);
 
                 // the energy can only be as large as
-                // the thinnest section connecting some vertex to vertices before it
-                let (row_start, row_end, row_len) = {
+                // the thinnest section connecting some vertex to vertices before it.
+                // this row is thus the longest connected subset of the
+                // grid line in parallel_dir_coords directions containing v,
+                // where every vertex has a neighbor in a esc_dir_coords direction with positive energy.
+                let (row_start, row_len) = {
+                    let v_coords = g.coordinates_of(v);
                     let mut v_left = v_coords;
                     let mut v_right = v_coords;
-                    let mut section_len = 1;
-                    let [left_step_fwd, right_step_fwd] = if v_dirs.nonempty() {
-                        [esc_dir_coords[1], esc_dir_coords[0]]
-                    } else {
-                        esc_dir_coords
-                    };
+                    let mut row_len = 0;
+                    let [left_step, right_step] = parallel_dir_coords;
+                    let [right_step_fwd, left_step_fwd] = esc_dir_coords;
                     loop {
-                        let next_left = v_left + parallel_dir_coords[0];
-                        let Some(index) = g.index_of(next_left) else {
+                        let Some(next_left) = g.try_wrap(v_left + left_step) else {
                             break;
                         };
-                        if min_cop_dist[index] < 2 {
+                        if min_cop_dist[g.unchecked_index_of(next_left)] < 2 {
                             break;
                         }
                         let Some(index_fwd) = g.index_of(next_left + left_step_fwd) else {
@@ -220,14 +222,13 @@ impl DilemmaNodes {
                             break;
                         }
                         v_left = next_left;
-                        section_len += 1;
+                        row_len += 1;
                     }
                     loop {
-                        let next_right = v_right + parallel_dir_coords[1];
-                        let Some(index) = g.index_of(next_right) else {
+                        let Some(next_right) = g.try_wrap(v_right + right_step) else {
                             break;
                         };
-                        if min_cop_dist[index] < 2 {
+                        if min_cop_dist[g.unchecked_index_of(next_right)] < 2 {
                             break;
                         }
                         let Some(index_fwd) = g.index_of(next_right + right_step_fwd) else {
@@ -237,44 +238,46 @@ impl DilemmaNodes {
                             break;
                         }
                         v_right = next_right;
-                        section_len += 1;
+                        row_len += 1;
                     }
-                    (v_left, v_right, section_len)
+                    debug_assert_eq!(g.try_wrap(v_left + row_len * right_step), Some(v_right));
+                    (v_left, row_len)
                 };
-                debug_assert_eq!(row_start + (row_len - 1) * parallel_dir_coords[1], row_end);
-                for i in 0..row_len {
-                    let v_coords = row_start + i * parallel_dir_coords[1];
-                    let index = g.unchecked_index_of(v_coords);
-                    let in_cone = cone_dirs[index].intersection(component_dirs).0.count_ones() >= 2;
+
+                // we update the energy of all vertices of the row at once,
+                // because now we know the row width.
+                'update_row: for i in 0..=row_len {
+                    let vi_coords = row_start + i * parallel_dir_coords[1];
+                    let Some(vi) = g.index_of(vi_coords) else {
+                        continue 'update_row;
+                    };
 
                     // what value shout this have,
                     // based on what values the neighbors in esc_dirs directions have
-                    let new_step_energy = if in_cone {
-                        let mut new_energy = isize::MIN;
-                        for n_coords in esc_dir_coords {
-                            if let Some(n) = g.index_of(v_coords + n_coords) {
-                                new_energy = isize::max(new_energy, self.energy[n]);
-                            }
+                    let Some(new_step_energy) = ({
+                        let n1 = g.index_of(vi_coords + esc_dir_coords[0]).map(|n| self.energy[n]);
+                        let n2 = g.index_of(vi_coords + esc_dir_coords[1]).map(|n| self.energy[n]);
+                        if cone_dirs[vi].intersection(component_dirs).0.count_ones() >= 2 {
+                            // in cone -> enough to have one neighbor with same function value
+                            Option::max(n1, n2)
+                        } else {
+                            // outside cone -> both neighbors must have energy and we lose energy
+                            Option::min(n1, n2).map(|e| e.saturating_sub(1))
                         }
-                        new_energy
-                    } else if let (Some(n1), Some(n2)) = (
-                        g.index_of(v_coords + esc_dir_coords[0]),
-                        g.index_of(v_coords + esc_dir_coords[1]),
-                    ) {
-                        isize::min(self.energy[n1], self.energy[n2]).saturating_sub(1)
-                    } else {
-                        isize::MIN
+                    }) else {
+                        continue 'update_row;
                     };
-                    let new_energy = isize::min(new_step_energy, row_len);
-                    if new_energy > self.energy[index] {
-                        self.energy[index] = new_energy;
+
+                    let new_energy = isize::min(new_step_energy, row_len + 1);
+                    if new_energy > self.energy[vi] {
+                        self.energy[vi] = new_energy;
                         if new_energy >= 0 {
-                            self.dilemma_regions[index] |= component_bit;
-                            self.dilemma_dirs[index].unionize(escape_dirs);
+                            self.dilemma_regions[vi] |= component_bit;
+                            self.dilemma_dirs[vi].unionize(escape_dirs);
                         }
 
                         queue.extend(
-                            esc_dir_coords.iter().filter_map(|&c| g.index_of(v_coords - c)),
+                            esc_dir_coords.iter().filter_map(|&c| g.index_of(vi_coords - c)),
                         );
                     }
                 }
@@ -301,7 +304,7 @@ impl DilemmaNodes {
         self.dilemma_regions.resize(edges.nr_vertices(), 0);
 
         self.energy.clear();
-        self.energy.resize(edges.nr_vertices(), 0);
+        self.energy.resize(edges.nr_vertices(), isize::MIN);
 
         self.dilemma_dirs.clear();
         self.dilemma_dirs.resize(edges.nr_vertices(), Dirs::EMPTY);
